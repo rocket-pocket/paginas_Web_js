74f9973cedb51b87b4b97f1cd7845786280b7d28
rocket-pocket <verdudaniel7@gmail.com>
2018-06-21 13:17:59 +0200

    datos

---
+++ HolaMundo.js
@@ -0,0 +1,20 @@
+//Declaración: var saludo -> Esta variable esta creada pero es undefined
+
+ //todas las palabras reservadas van con minusculas
+
+                                        /* En JavaScript hay 5TIPOS DE DATOS
+                                            -STRING
+                                            -NUMBER
+                                            -BOOLEANS (TRUE / FALSE)
+                                            -OBJECT (TODO LO QUE NO SEA NINGUNO DE LOS TRES ANTERIORES)
+                                            -UNDEFINED
+                                        */           
+/* Inicialización: saludo = hola */
+
+
+/* declaracion + inicialización */
+var saludo = 'Hello'
+var user = 'Mundo'
+
+console.log(saludo, user)
+                    

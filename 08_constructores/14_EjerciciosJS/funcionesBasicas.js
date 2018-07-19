5ea446ac7c23518d915ebb41d5f12895f72d91d2
rocket-pocket <verdudaniel7@gmail.com>
2018-07-19 12:22:37 +0200

    ejercicios comienzo

---
+++ 08_constructores/14_EjerciciosJS/funcionesBasicas.js
@@ -0,0 +1,7 @@
+//funcion saludar
+function saludar(){
+    let nombre1 = 'Pepe'
+    let nombre2 = 'Juan'
+    console.log(`hola mi nombre es ${nombre1} y el nombre de mi amigo es ${nombre2}`)
+}
+saludar()//hola mi nombre es Pepe y el nombre de mi amigo es Juan

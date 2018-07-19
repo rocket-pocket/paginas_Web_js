7cff55e07a4c7e705f8f2536bec2f194f0d2894f
rocket-pocket <verdudaniel7@gmail.com>
2018-07-19 12:27:31 +0200

    FUNCION LOOP SIN TERMINAR

--- 08_constructores/14_EjerciciosJS/funcionesBasicas.js
+++ 08_constructores/14_EjerciciosJS/funcionesBasicas.js
@@ -5,3 +5,14 @@ function saludar(){
     console.log(`hola mi nombre es ${nombre1} y el nombre de mi amigo es ${nombre2}`)
 }
 saludar()//hola mi nombre es Pepe y el nombre de mi amigo es Juan
+
+//funcion loop
+/* function loop(x){
+    let x = '#'
+    for (let i = 0; i < 8; i++) {
+        console.log(x)
+        
+    }
+}   REVISAR-------------------->
+
+loop() */

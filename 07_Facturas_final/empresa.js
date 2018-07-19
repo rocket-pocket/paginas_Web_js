ee3c9b208a632f8e4ba189bc1e0a39c1aff426d6
rocket-pocket <verdudaniel7@gmail.com>
2018-07-19 13:59:06 +0200

    07_reestructurar

--- 07_Facturas_final/empresa.js
+++
@@ -1,8 +0,0 @@
-export class Empresa{
-    constructor(nombre, direccion, telefono, nif) {
-    this.nombre = nombre 
-    this.direccion = direccion 
-    this.telefono = telefono 
-    this.nif = nif 
-    } 
-}
